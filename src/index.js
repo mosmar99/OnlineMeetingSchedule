9b6d996a401aea177b922de68752bef403ed4e10
mosmar99 <47375043+mosmar99@users.noreply.github.com>
2023-09-18 12:14:13 +0100

    useFetch hook
    - upon loading a url it renders data but does some checks in case of failure
    
    1. checks that url returns something
    2. make sure that which is returned is "ok", doesn't return some errors, if so, prints the error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import { App } from "./app";
 import { createRoot } from "react-dom/client"; 
+import React from "react";
 
 const mainContainer = document.createElement("div");
 document.body.append(mainContainer);
 
 const root = createRoot(mainContainer);
-root.render(<App />)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>    
+);
