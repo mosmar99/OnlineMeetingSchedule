924f27a536df95ef9d2a649c3f54a9b900a8becf
mosmar99 <47375043+mosmar99@users.noreply.github.com>
2023-09-19 21:16:35 +0100

    tried to fix merge conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,67 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const InterpolateHtmlPlugin = require("interpolate-html-plugin");
 const path = require("path");
 
-module.exports = {
-    entry: {
-        main: path.resolve(__dirname, "./src/index.js")
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "deploy"),
-    },
-    devServer: {
-        open: true,
-        port: 3000,
-        historyApiFallback: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Online meeting schedule",
-        }),
-        new CleanWebpackPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: { loader: "babel-loader" }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: "asset/resource"
-            }
-            
-         ],
+module.exports = (_, argv) => {
+    let public_url = argv.mode === "development" ? "file:/"+path.resolve(__dirname, "public") : "<future-url>"
+
+    console.log("PUBLIC_URL:", public_url);
+    console.log("MODE:", argv.mode);
+    console.log("")  // console linebreak
+
+    const config = {
+        entry: {
+            main: path.resolve(__dirname, "./src/index.js")
+        },
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "deploy"),
+        },
+        devServer: {
+            open: true,
+            port: 3000,
+            historyApiFallback: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: "public/index.html"
+            }),
+            new InterpolateHtmlPlugin({
+                PUBLIC_URL: public_url
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: "public",
+                        globOptions: {
+                            ignore: [ "**/*.html" ]
+                        }
+                    }
+                ]
+            }),
+            new CleanWebpackPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: { loader: "babel-loader" }
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
+                },
+                {
+                    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                    type: "asset/resource"
+                }
+            ],
+        }
     }
-};
+
+    return config;
+}
