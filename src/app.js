602e1a799b25d1fe5776765843a52b1fc386ceb8
Neobyte01 <neobyte01@hotmail.com>
2023-09-21 12:33:09 +0200

    Added barebones calendar page

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import "./app.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import NewMeet from "./pages/NewMeet";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Calendar from "./pages/Calendar";
 
 export const App = () => {
     return (
@@ -15,6 +16,7 @@ export const App = () => {
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/newMeet' element={<NewMeet/>}/>
+                <Route path='/calendar' element={<Calendar />}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/signup' element={<SignUp/>}/>
             </Routes>
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,9 +1,146 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, List, ListItem, ListItemText, Button } from '@mui/material';
+
+const meetings = [
+    {
+        title: "Meeting 1",
+        description: "Description of meeting 1",
+        location: "Somewhere",
+        startDate: new Date("2023-09-23T15:00:00"),
+        endDate: new Date("2023-09-23T15:15:00") 
+    },
+    {
+        title: "Meeting 2",
+        description: "Description of meeting 2",
+        location: "Somewhere",
+        startDate: new Date("2023-10-29T16:00:00"),
+        endDate: new Date("2023-10-29T17:45:00") 
+    }
+]
+
+const dayNames = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+const monthNames = ["January", "Feburary", "Mars", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+/**
+ * Formats the start and end date for a meeting nicely.
+ * @param {*} startDate - meeting start date
+ * @param {*} endDate   - meeting end date (same calendar date as start)
+ * @returns formatted meeting date and time
+ */
+const displayDates = (startDate, endDate) => {
+    const calendarDate = `${1900+startDate.getYear()}/${startDate.getMonth()}/${startDate.getDate()}` 
+    
+    const displayTime = date => `${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`
+
+    const startTime = displayTime(startDate);
+    const endTime = displayTime(endDate);
+
+    return `${startTime} - ${endTime}, ${calendarDate}`;
+}
+
+/**
+ * Fetch the dates inside a month and a few surrounding dates to
+ * construct an even sunday to saturday month structure (i.e a
+ * visual month).
+ * @param {Date} selectedMonth  - Date with selected month (any day within the month)
+ * @returns All dates (days) within the visual month.
+ */
+const getVisualMonth = selectedMonth => {
+    let year = selectedMonth.getYear() + 1900;
+    let month = selectedMonth.getMonth();
+
+    let daysInMonth = new Date(year, month+1, 0).getDate();
+
+    let startDate = new Date(year, month, 1);           // Start of month date
+    let endDate = new Date(year, month, daysInMonth);   // End of month date
+    
+    startDate.setDate(startDate.getDate() - startDate.getDay());    // Adjust start date to sunday
+    endDate.setDate(endDate.getDate() + (6-endDate.getDay()));      // Adjust end date to saturday
+
+    let datesArray = []  // Every date (day) inside the visual month.
+
+    for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1))
+        datesArray.push(new Date(date));
+
+    return datesArray;
+}
+
+/**
+ * Calendar component made with React
+ */
 const Calendar = () => {
+    const [viewedMonth, setViewedMonth] = useState(new Date());
+
+    const visualMonth = getVisualMonth(viewedMonth);
+
+    const nextMonth = () => {
+        let month = new Date();
+        month.setMonth(viewedMonth.getMonth()+1);
+        console.log(month);
+        setViewedMonth(month);
+    }
+
+    const prevMonth = () => {
+        let month = new Date();
+        month.setMonth(viewedMonth.getMonth()-1);
+        console.log(month);
+        setViewedMonth(month);
+    }
+
     return ( 
-        <div className="calendar">
-            
-        </div>
+        <Grid container>
+            <Grid item xs={3}>
+                <h2 style={{ margin: "30px 0 5px 15px"}}>Upcoming meetings</h2>
+                <List>
+                    {meetings.map(meeting => (
+                        <ListItem key={meeting.title}>
+                            <ListItemText 
+                                primary={meeting.title} 
+                                secondary={displayDates(meeting.startDate, meeting.endDate)} 
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+                <Button component={Link} to="/newMeet" style={{ margin: "30px 0 5px 15px"}}>+ New meeting</Button> 
+            </Grid>
+            <Grid item xs={9} padding={2}>
+                <div>
+                    <h1>{monthNames[viewedMonth.getMonth()]}</h1>
+                    <Grid container>
+                        <Button variant="outlined" onClick={prevMonth}>Previous</Button>
+                        <Button variant="outlined" onClick={nextMonth}>Next</Button>
+                    </Grid>
+                </div>
+                <Grid container columns={7} style={{ marginTop: "20px"}}>
+                    {dayNames.map(day => (
+                        <Grid item xs={1} key={day}>
+                            <p><b>{day}</b></p>
+                        </Grid>
+                    ))}
+                    {visualMonth.map(date => {
+                        let extraStyle = {};
+                        
+                        if (date.getMonth() !== viewedMonth.getMonth()) 
+                            extraStyle = {opacity: "0.4", ...extraStyle}
+
+                        let meetingsToday = meetings.filter(meeting => meeting.startDate.toDateString() === date.toDateString());
+
+                        return (
+                            <Grid item xs={1} key={date}>
+                                <div style={{height: "100px", border: "1px solid lightgray"}}>
+                                    <div style={{padding: "10px"}}>
+                                        <span style={extraStyle}>{date.getDate()}</span>
+                                        {meetingsToday.map(meeting => <p key={meeting.title}>{meeting.title}</p>)}
+                                    </div>
+                                </div>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
- 
+
 export default Calendar;
