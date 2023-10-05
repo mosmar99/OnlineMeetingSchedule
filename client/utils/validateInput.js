7cc41390906ccbbb4297423536dc67bc29fbfb38
Isac Paulsson <isac.97@live.se>
2023-10-05 15:31:59 +0200

    added password requirements

--- client/utils/validateInput.js
+++ client/utils/validateInput.js
@@ -1,13 +1,23 @@
 
 /**
  * @param {string} email 
- * @returns {boolean}
+ * @returns {boolean,}
  */
 const isValidEmail = (email) => {
     const exp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ig; // not perfect but works for now
     return exp.test(email);
 }
 
+/**
+ * @param {string} password
+ * @returns {boolean, string}
+ */
+const isValidPassword = (password) => {
+    let exp = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[a-zA-Z])[a-zA-Z-\#\$\.\%\&\*\d]{8,16}$/g
+    return exp.test(password);
+}
+
 module.exports = {
-    isValidEmail
+    isValidEmail,
+    isValidPassword
 }
